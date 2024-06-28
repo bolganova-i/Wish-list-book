a42c834a8310d1b63d6beab2dc9d917b97b3c83c
Ирина Болганова <youremail@example.com>
2024-06-28 23:04:01 +0300

    Changes have been made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { data } from "./data";
 import './App.css';
 
 function App() {
+
+  const [gifts, setGifts] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+          <h1>Wish list books - {gifts.length} 🎁</h1>
+      </div>
+
+      {gifts.map((element => {
+
+        const {id, nameBook, image, price} = element;
+
+        const removeGift = (id) => {
+          
+          let newGifts = gifts.filter(nameBook => nameBook.id !== id);
+        
+          setGifts(newGifts);
+        
+        }
+
+        return (
+          <div key={id}>
+            <div className="container">
+                <h2>{id} - {nameBook}</h2>
+            </div>
+
+            <div className="container">
+              <img src={image} width="150px" alt="book"/>
+            </div>
+
+            <div className="container">
+              <h3>{price}</h3>
+            </div>
+
+            <div className="container">
+              <button onClick={() => removeGift(id)}>Remove</button>
+            </div>
+          </div>
+        )
+      }))}
+      <div className="container">
+          <button onClick={() => setGifts([])}>Delete all</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const data = [
+
+    {
+        id: 1,
+        nameBook: "Heaven Official's Blessing",
+        image: "https://images.unsplash.com/photo-1532012197267-da84d127e765?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D",
+        price: "6 volumes - $ 125"
+    },
+    
+    {
+        id: 2,
+        nameBook: "War and Peace",
+        image: "https://images.unsplash.com/photo-1495640388908-05fa85288e61?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGJvb2t8ZW58MHx8MHx8fDA%3D",
+        price: "4 volumes - $ 25"
+    },
+    
+    {
+        id: 3,
+        nameBook: "Pride and Prejudice",
+        image: "https://images.unsplash.com/photo-1579847195281-b78bef1e0cac?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDR8fEhlYXZlbiUyME9mZmljaWFsJ3MlMjBCbGVzc2luZyUyMGJvb2tzfGVufDB8fDB8fHww",
+        price: "$ 18"
+    },
+    
+    {
+        id: 4,
+        nameBook: "Harry Potter ",
+        image: "https://images.unsplash.com/photo-1600189261867-30e5ffe7b8da?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8SGFycnklMjBQb3R0ZXIlMjAlMjBib29rc3xlbnwwfHwwfHx8MA%3D%3D",
+        price: "7 volumes - $ 250"
+    },
+
+    {
+        id: 5,
+        nameBook: "The Untamed",
+        image: "https://images.unsplash.com/photo-1535905496755-26ae35d0ae54?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzl8fEhlYXZlbiUyME9mZmljaWFsJ3MlMjBCbGVzc2luZyUyMGJvb2tzfGVufDB8fDB8fHww",
+        price: "3 volumes - $ 100"
+    },
+
+    {
+        id: 6,
+        nameBook: "Twilight",
+        image: "https://images.unsplash.com/photo-1541963463532-d68292c34b19?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D",
+        price: "4 volumes - $ 45"
+    }
+]
